c510690a6a60569a58d1fb11acc178b352ceda83
Shruti Joshi <shruti.joshi@tuebingen.mpg.de>
2022-02-25 19:21:31 -0500

    fix stupid mistake

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-SBATCH --time=16:00:00
-SBATCH --account=rrg-ebrahimi
-SBATCH --mem=48G
-SBATCH --gres=gpu:1
-SBATCH --array=1-5
-SBATCH --output=out/%x/%A_%a.out
-SBATCH --cpus-per-task=4
-SBATCH --mail-type=BEGIN
-SBATCH --mail-type=END
-SBATCH --mail-type=FAIL
+# SBATCH --time=16:00:00
+# SBATCH --account=rrg-ebrahimi
+# SBATCH --mem=48G
+# SBATCH --gres=gpu:1
+# SBATCH --array=1-5
+# SBATCH --output=out/%x/%A_%a.out
+# SBATCH --cpus-per-task=4
+# SBATCH --mail-type=BEGIN
+# SBATCH --mail-type=END
+# SBATCH --mail-type=FAIL
 
 source /home/shrutij/venvs/bbs/bin/activate
 wandb offline
